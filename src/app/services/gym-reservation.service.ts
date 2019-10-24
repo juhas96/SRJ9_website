686cdd3b4741ce5766462c7d8f21c5ba6ba3b269
Jakub Juhas <juhas.jugi@gmail.com>
2019-10-24 13:13:58 +0200

    Changes

--- src/app/services/gym-reservation.service.ts
+++ src/app/services/gym-reservation.service.ts
@@ -14,32 +14,33 @@ export class GymReservationService {
 
   // GET /api/gym_reservation
   public getReservations(): Observable<GymReservation[]> {
-    return this.httpService.get<GymReservation[]>('http://147.232.191.144:8087/api/gym_reservation');
+    return this.httpService.get<GymReservation[]>('http://localhost:8087/api/gym_reservation');
   }
 
   // GET /api/gym_reservation/id
   public getReservationById(id: Number): Observable<GymReservation> {
-    return this.httpService.get<GymReservation>('http://147.232.191.144:8087/api/gym_reservation/' + id);
+    return this.httpService.get<GymReservation>('http://localhost:8087/api/gym_reservation/' + id);
   }
 
   // GET /api/gym_reservation/current_week
   public getReservationForCurrentWeek(): Observable<GymReservation[]> {
-    return this.httpService.get<GymReservation[]>('http://147.232.191.144:8087/api/gym_reservation/current_week');
+    return this.httpService.get<GymReservation[]>('http://localhost:8087/api/gym_reservation/current_week');
   }
 
   // POST /api/gym_reservation
   public createNewGymReservation(gymReservation: GymReservation): Observable<GymReservation> {
-    return this.httpService.post<GymReservation>('http://147.232.191.144:8087/api/gym_reservation', gymReservation); //httpOptions ? 
+    return this.httpService.post<GymReservation>('http://localhost:8087/api/gym_reservation', gymReservation); //httpOptions ? 
   }
 
   // DELETE /api/gym_reservation/id
-  public deleteGymReservation(id: Number): Observable<GymReservation> {
-    return this.httpService.delete<GymReservation>('http://147.232.191.144:8087/api/gym_reservation/' + id)
+  public deleteGymReservation(id: number): Observable<GymReservation> {
+    console.log('deleted' + id);
+    return this.httpService.delete<GymReservation>('http://localhost:8087/api/gym_reservation/' + id);
   }
 
   // PUT /api/gym_reservation/id
   public updateGymReservation(id: Number, gymReservation: GymReservation): Observable<GymReservation> {
-    return this.httpService.put<GymReservation>('http://147.232.191.144:8087/api/gym_reservation/' + id, gymReservation) //httpOptions ?
+    return this.httpService.put<GymReservation>('http://localhost:8087/api/gym_reservation/' + id, gymReservation) //httpOptions ?
   }
 
 
