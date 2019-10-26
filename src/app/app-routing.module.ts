fdbf8e79789f3348b930a0b1b0653c1d25713989
Matej Godočík <godocik@novitechgroup.sk>
2019-10-26 23:24:19 +0200

    tables for gym reservation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './auth/register/sign-up/sign-up.component';
 import { LogInComponent } from './auth/login/log-in/log-in.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { GymReservationComponent } from './pages/gym-reservation/gym-reservation.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LogInComponent},
   { path: 'signup', component: SignUpComponent},
-  { path: 'admin/dashboard', component: DashboardComponent}
+  { path: 'admin/dashboard', component: DashboardComponent},
+  { path: 'gym_reservation', component: GymReservationComponent}
 ];
 
 @NgModule({
--- src/app/pages/gym-reservation/gym-reservation.component.ts
+++ src/app/pages/gym-reservation/gym-reservation.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { GymReservationService } from 'src/app/services/gym-reservation.service';
+import { GymReservation } from 'src/app/model/gym.model';
+import { filter } from 'rxjs/operators'
+import * as moment from 'moment'
+
 
 @Component({
   selector: 'app-gym-reservation',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymReservationComponent implements OnInit {
 
-  constructor() { }
+  gridStyle = {
+    width: '25%',
+    textAlign: 'center'
+  };
+
+
+  gymTimes = ["21:00 - 21:59", "22:00 - 22:59", "23:00 - 23:59", "00:00 - 00:59"]
+  sundayDate = new Date();
+  arr:GymReservation[];
+  arr2:GymReservation[];
+  menic: GymReservation
+
+  
+
+  constructor(private gymService: GymReservationService) {
+   }
 
   ngOnInit() {
+
+    this.gymService.getReservationForCurrentWeek().subscribe(data => {
+      //this.arr=data;
+      this.arr = data.filter(res => res.id==6 || res.id==33)
+      console.log(this.arr);
+      
+    })
+
+    console.log(moment().day(0).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //0 pre nedelu
+    console.log(moment().day(2).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //2 pre utorok
+    console.log(moment().day(4).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //4 pre stvrtok
+
+    console.log(moment().day(7).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //7 pre buducu nedelu
+    console.log(moment().day(9).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //9 pre buduci utorok
+    console.log(moment().day(11).hour(0).minute(0).second(0).millisecond(0).format("YYYY-MM-DDTHH:mm:ss.SSS+0000")) //11 pre buduci stvrtok
+  }
+
+
+  fk(id){
+    this.menic = this.arr.filter(el => el.id==id)[0]
+    console.log(this.menic)
+    setTimeout( () => { 
+      this.menic.status = "zmenene"
+    console.log(this.menic)
+    }, 1000)
+    
   }
 
 }
