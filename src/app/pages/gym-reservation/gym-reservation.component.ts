4b4335bf320dbaa19fb35842daa21b3e1660ac68
Matej Godočík <godocik@novitechgroup.sk>
2019-11-09 22:58:32 +0100

    fixed date values

--- src/app/pages/gym-reservation/gym-reservation.component.ts
+++ src/app/pages/gym-reservation/gym-reservation.component.ts
@@ -17,32 +17,28 @@ import {CookieService} from 'ngx-cookie-service';
 })
 export class GymReservationComponent implements OnInit {
 
-  /* gridStyle = {
-    width: '25%',
-    textAlign: 'center'
-  }; */
-
+  // variable for loading spinner
   isSpinning = false;
 
   // dates and arrays for current sunday and next sunday
-  sundayDate = moment().day(0).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // zero for sunday
-  nextSundayDate = moment().day(7).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // seven (0+7) for next sunday
-  sundayDateTable = moment().day(0).format('DD.MM.YYYY'); // variable for writing sunday date in table e.g. 20.10.2010
-  nextSundayDateTable = moment().day(7).format('DD.MM.YYYY');
+  sundayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(7) : this.setReservationDate(0);
+  nextSundayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(14) : this.setReservationDate(7);
+  sundayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(7) : this.setTableDate(0);
+  nextSundayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(14) : this.setTableDate(7);
   sundayArray: GymReservation[];
   nextSundayArray: GymReservation[];
 
-  tuesdayDate = moment().day(2).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // two for tuesay
-  nextTuesdayDate = moment().day(9).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // nine (2+7) for next tuesday
-  tuesdayDateTable = moment().day(2).format('DD.MM.YYYY');
-  nextTuesdayDateTable = moment().day(9).format('DD.MM.YYYY');
+  tuesdayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(9) : this.setReservationDate(2);
+  nextTuesdayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(16) : this.setReservationDate(9);
+  tuesdayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(9) : this.setTableDate(2);
+  nextTuesdayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(16) : this.setTableDate(9);
   tuesdayArray: GymReservation[];
   nextTuesdayArray: GymReservation[];
 
-  thursdayDate = moment().day(4).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // four for thursday
-  nextThursdayDate = moment().day(11).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000'); // eleven (4+7) for next thursday
-  thursdayDateTable = moment().day(4).format('DD.MM.YYYY');
-  nextThursdayDateTable = moment().day(11).format('DD.MM.YYYY');
+  thursdayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(11) : this.setReservationDate(4);
+  nextThursdayDate = (moment().day() === 5 || moment().day() === 6) ? this.setReservationDate(18) : this.setReservationDate(11);
+  thursdayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(11) : this.setTableDate(4);
+  nextThursdayDateTable = (moment().day() === 5 || moment().day() === 6) ? this.setTableDate(18) : this.setTableDate(11);
   thursdayArray: GymReservation[];
   nextThursdayArray: GymReservation[];
 
@@ -63,19 +59,14 @@ export class GymReservationComponent implements OnInit {
   ngOnInit() {
 
     this.gymService.getReservations().subscribe(data => {
-      this.sundayArray = data.filter(value => value.date == this.sundayDate);
-      this.nextSundayArray = data.filter(value => value.date == this.nextSundayDate);
-      this.sundayArray.sort( (a, b) => a.id - b.id);
+      this.sundayArray = data.filter(value => value.date === this.sundayDate);
+      this.nextSundayArray = data.filter(value => value.date === this.nextSundayDate);
 
-      this.tuesdayArray = data.filter(value => value.date == this.tuesdayDate);
-      this.nextTuesdayArray = data.filter(value => value.date == this.nextTuesdayDate);
-      console.log('utorok datum je ' + this.nextTuesdayDate);
-      console.log(this.tuesdayArray);
-      this.thursdayArray = data.filter(value => value.date == this.thursdayDate);
-      this.nextThursdayArray = data.filter(value => value.date == this.nextThursdayDate);
-      console.log('stvrtok datum - ' + this.nextThursdayDate);
-      console.log(this.nextThursdayArray);
+      this.tuesdayArray = data.filter(value => value.date === this.tuesdayDate);
+      this.nextTuesdayArray = data.filter(value => value.date === this.nextTuesdayDate);
 
+      this.thursdayArray = data.filter(value => value.date === this.thursdayDate);
+      this.nextThursdayArray = data.filter(value => value.date === this.nextThursdayDate);
     });
   }
 
@@ -138,6 +129,16 @@ export class GymReservationComponent implements OnInit {
     if (email) {
       return email.substring(0, email.indexOf('@'));
     }
+  }
+
+  // this function returns specific date format in string by given day of week (e.g. for day=0 it returns current sunday, 7 for next sunday)
+  setReservationDate(day: number): string{
+    return  moment().day(day).hour(1).minute(0).second(0).millisecond(0).format('YYYY-MM-DDTHH:mm:ss.SSS+0000');
+  }
+
+  // this function returns specific date format in string for tables by gjven day of week (e.g. 0 for sunday, 7 for next sunday)
+  setTableDate(day: number): string{
+    return moment().day(day).format('DD.MM.YYYY');
   }
 
 
