1ad771bd4f687d951085bca693fd592d88771fa6
Jakub Juhas <juhas.jugi@gmail.com>
2020-06-22 00:40:02 +0200

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,4 @@
 export const environment = {
   production: true,
 
-  firebaseConfig: {
-    apiKey: 'AIzaSyAKP_PjXsF17OFbNq90f0M_ruMtmpqMgjs',
-    authDomain: 'room-reservations-srj9.firebaseapp.com',
-    databaseURL: 'https://room-reservations-srj9.firebaseio.com',
-    projectId: 'room-reservations-srj9',
-    storageBucket: 'room-reservations-srj9.appspot.com',
-    messagingSenderId: '764403866254',
-    appId: '1:764403866254:web:75359acd6142a8d44e87df',
-    measurementId: 'G-YQM1E8KP64'
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,7 @@
 export const environment = {
   production: false,
 
-  firebaseConfig: {
-    apiKey: 'AIzaSyAKP_PjXsF17OFbNq90f0M_ruMtmpqMgjs',
-    authDomain: 'room-reservations-srj9.firebaseapp.com',
-    databaseURL: 'https://room-reservations-srj9.firebaseio.com',
-    projectId: 'room-reservations-srj9',
-    storageBucket: 'room-reservations-srj9.appspot.com',
-    messagingSenderId: '764403866254',
-    appId: '1:764403866254:web:75359acd6142a8d44e87df',
-    measurementId: 'G-YQM1E8KP64'
-  }
+
 };
 
 /*
